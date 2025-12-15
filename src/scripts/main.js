7076c4eb027e4e0251b133d6222e06a84e2f9268
Oleh Prokopenko <olehprokopenko1@gmail.com>
2025-12-15 13:53:00 +0200

    tusk solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,45 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(
+      eventName,
+      (ev) => {
+        if (eventName === 'click') {
+          resolve(
+            `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}`,
+          );
+        }
+
+        if (eventName === 'contextmenu') {
+          ev.preventDefault();
+
+          resolve(
+            `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}`,
+          );
+        }
+
+        if (eventName === 'blur') {
+          ev.preventDefault();
+
+          resolve(
+            `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}`,
+          );
+        }
+      },
+      { once: true },
+    );
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+
+  div.textContent = message;
+
+  document.body.append(div);
 };
 
 const loginField = document.getElementById('login');
